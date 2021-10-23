70c957d0aa6eed4ab79c6988fdb810053593396e
michael alexakos <93053477+mixalissan@users.noreply.github.com>
2021-10-23 21:50:00 +0300

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "o andreas gamietai"
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+#dn exw idea pws douleuei, alla me auto kanoume ping to server kathe 5 lepta gia na mh kleisei 
+# kalh fash 
+#brb
